9d19f4762b5c979f86543ded580ae2be92ef6cbd
Huzaifa Iqbal <huzaifaiqbal89@gmail.com>
2024-11-29 21:53:07 +0500

    firebase integration updated

--- src/screens/user/Settings.jsx
+++ src/screens/user/Settings.jsx
@@ -1,66 +1,80 @@
-import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
-import { generalFontSize, GlobalStyle, textColor, themeColor, whiteColor } from '../../styles/Theme'
-import ProfileCard from '../../components/ProfileCard'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faBell, faBox, faCheck, faCheckCircle, faMotorcycle, faUser } from '@fortawesome/free-solid-svg-icons'
-import { useSelector } from 'react-redux'
-import { store } from '../../Redux/Store'
-import { SET_NOTIFICATION_STATUS } from '../../Redux/Store/Slices/General'
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { store } from '../../Redux/Store';
+import { SET_FCM_TOKEN, SET_NOTIFICATION_STATUS } from '../../Redux/Store/Slices/General';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBell, faBox, faCheckCircle, faMotorcycle, faUser } from '@fortawesome/free-solid-svg-icons';
+import { generalFontSize, GlobalStyle, textColor, themeColor, whiteColor } from '../../styles/Theme';
+import { handleNotificationPermission } from '../../utils/Utils';
 
 const Settings = () => {
-    const notiStatus = useSelector((state) => state.general.isNotificationActive)
-    const handleNotificationStatus = () => {
-        notiStatus === true ? store.dispatch(SET_NOTIFICATION_STATUS(false)) : store.dispatch(SET_NOTIFICATION_STATUS(true))
-    }
-    const [isNotiActive, setIsNotiActive] = useState(false)
+    const notiStatus = useSelector((state) => state.general.isNotificationActive);
+
+    const handleNotificationStatus = async () => {
+        if (notiStatus) {
+            // Turn off notifications
+            store.dispatch(SET_NOTIFICATION_STATUS(false));
+            store.dispatch(SET_FCM_TOKEN(null)); // Clear FCM token
+            console.log('Notifications turned off.');
+        } else {
+            // Turn on notifications
+            await handleNotificationPermission();
+        }
+    };
+
     return (
         <SafeAreaView style={GlobalStyle.pageWrapper}>
             <ScrollView>
                 <View style={GlobalStyle.container}>
-                    <ProfileCard />
                     <View style={GlobalStyle.pageHeadingCont}>
                         <Text style={GlobalStyle.pageHeading}>Order Size</Text>
                     </View>
                     <View style={styles.menu}>
                         <TouchableOpacity style={styles.menuItem}>
                             <FontAwesomeIcon icon={faUser} size={generalFontSize + 4} color={themeColor} />
-                            <Text style={styles.menuText}>
-                                Profile
-                            </Text>
+                            <Text style={styles.menuText}>Profile</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.menuItem}>
                             <FontAwesomeIcon icon={faBox} size={generalFontSize + 4} color={themeColor} />
-                            <Text style={styles.menuText}>
-                                Orders
-                            </Text>
+                            <Text style={styles.menuText}>Orders</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={handleNotificationStatus} style={[styles.menuItem, notiStatus && (styles.notiItem)]}>
-                            <FontAwesomeIcon icon={faBell} size={generalFontSize + 4} color={notiStatus ? whiteColor : themeColor} />
+                        <TouchableOpacity
+                            onPress={handleNotificationStatus}
+                            style={[styles.menuItem, notiStatus && styles.notiItem]}
+                        >
+                            <FontAwesomeIcon
+                                icon={faBell}
+                                size={generalFontSize + 4}
+                                color={notiStatus ? whiteColor : themeColor}
+                            />
                             <Text style={[styles.menuText, { color: notiStatus ? whiteColor : textColor }]}>
                                 Notifications
                             </Text>
-                            <FontAwesomeIcon icon={faCheckCircle} size={generalFontSize + 4} color={notiStatus ? whiteColor : themeColor} style={{ marginLeft: 'auto' }} />
+                            <FontAwesomeIcon
+                                icon={faCheckCircle}
+                                size={generalFontSize + 4}
+                                color={notiStatus ? whiteColor : themeColor}
+                                style={{ marginLeft: 'auto' }}
+                            />
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.menuItem}>
                             <FontAwesomeIcon icon={faMotorcycle} size={generalFontSize + 4} color={themeColor} />
-                            <Text style={styles.menuText}>
-                                Become A Vendor
-                            </Text>
+                            <Text style={styles.menuText}>Become A Vendor</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
-    )
-}
+    );
+};
 
-export default Settings
+export default Settings;
 
 const styles = StyleSheet.create({
     menu: {
         gap: 20,
-        marginBottom: 50
+        marginBottom: 50,
     },
     menuItem: {
         backgroundColor: whiteColor,
@@ -74,15 +88,15 @@ const styles = StyleSheet.create({
         elevation: 10,
         flexDirection: 'row',
         alignItems: 'center',
-        gap: 20
+        gap: 20,
     },
     menuText: {
         fontSize: generalFontSize + 2,
         color: textColor,
         fontFamily: 'Axiforma-Regular',
-        fontWeight: "400"
+        fontWeight: '400',
     },
     notiItem: {
         backgroundColor: themeColor,
-    }
-})
+    },
+});
--- src/utils/Utils.jsx
+++ src/utils/Utils.jsx
@@ -1,5 +1,11 @@
 // timeUtils.js
 
+import messaging from '@react-native-firebase/messaging';
+import { store } from "../Redux/Store"
+import { SET_FCM_TOKEN, SET_NOTIFICATION_STATUS } from '../Redux/Store/Slices/General';
+import { PermissionsAndroid, Alert } from 'react-native';
+import notifee from "@notifee/react-native"
+
 // Function to format time as 12-hour format with AM/PM
 export const formatTime = (date) => {
     let hours = date.getHours()
@@ -25,3 +31,51 @@ export const isValidTime = (time) => {
     const hours = time.getHours()
     return hours >= 9 && hours <= 17 // Ensure time is between 9 AM and 5 PM
 }
+
+export const handleNotificationPermission = async () => {
+    try {
+        // Request notification permissions
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        );
+
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log('Notification permission granted.');
+            store.dispatch(SET_NOTIFICATION_STATUS(true));
+
+            // Fetch and store FCM token
+            const token = await messaging().getToken();
+            if (token) {
+                console.log('FCM Token:', token);
+                store.dispatch(SET_FCM_TOKEN(token));
+            } else {
+                console.log('No FCM token found.');
+            }
+        } else {
+            console.log('Notification permission denied.');
+            store.dispatch(SET_NOTIFICATION_STATUS(false));
+        }
+    } catch (error) {
+        console.error('Error handling notification permission:', error);
+    }
+};
+
+export const handleForegroundNotification = () => {
+    messaging().onMessage(async (remoteMessage) => {
+        const noti = JSON.stringify(remoteMessage.notification);
+        console.log(noti)
+        // Alert.alert('New Notification!', JSON.stringify(remoteMessage.notification));
+        // Create a channel (required for Android)
+        const channelId = await notifee.createChannel({
+            id: 'default',
+            name: 'Default Channel',
+        });
+
+        // Display a notification
+        await notifee.displayNotification({
+            title: 'noti?.title',
+            body: 'noti?.body',
+            
+        });
+    });
+};
